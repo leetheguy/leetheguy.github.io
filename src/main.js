68045a5e240e2ee975b1fef0383e32ad87c09240
lee <leetheguy@gmail.com>
2014-05-26 09:49:24 -0700

    init

---
+++ src/main.js
@@ -0,0 +1,40 @@
+//    Copyright (C) 2014  Lee Nathan
+//    https://github.com/leetheguy/ternion
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+var cjs = createjs;
+var stage;
+var architectureTiles;
+var dirt, rock;
+
+function init() {
+    stage = new cjs.Stage("demoCanvas");
+    cjs.Ticker.addEventListener("tick", tick);
+
+    var sprites = {
+        images: ["./assets/images/architecture.png"],
+        frames: {width:32, height:32},
+        animations: {dirt_1: 0, rock: 1, dirt_2: 16}
+    };
+    architectureTiles = new cjs.SpriteSheet(sprites);
+    dirt = new cjs.Sprite(architectureTiles, "dirt_2");
+    rock = new cjs.Sprite(architectureTiles, "dirt_1");
+    rock.x = 32;
+    stage.addChild(dirt, rock);
+}
+
+function tick(event) {
+    stage.update();
+}
